f9fae6750c5f89a3db94251b4476977c7ed054df
Annette Mili Kemppi <annette.kemppi@helsinki.fi>
2016-05-04 19:24:33 +0000

    nitrous-backup at Wed May  4 19:24:33 UTC 2016

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,7 @@
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+def display_rainbow(colour)
+  display_colors(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+end
+
 # Write your #display_rainbow method here
